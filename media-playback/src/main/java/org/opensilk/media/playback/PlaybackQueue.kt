29ca039165bc0f3b1c5e4f600d19b1573c4b4818
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2017-03-12 17:45:03 -0500

    orpheus: move to playback queue

--- media-playback/src/main/java/org/opensilk/media/playback/PlaybackQueue.kt
+++ media-playback/src/main/java/org/opensilk/media/playback/PlaybackQueue.kt
@@ -1,11 +1,28 @@
 package org.opensilk.media.playback
 
+import android.media.MediaDescription
 import android.media.browse.MediaBrowser
 import android.media.session.MediaSession.*
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicLong
 
 /**
  * Created by drew on 2/24/17.
  */
 class PlaybackQueue {
 
+    private val mIdGen = AtomicLong(1)
+
+    companion object {
+        val EMPTY_DESCRIPTION: MediaDescription = MediaDescription.Builder().build()
+        val EMPTY_ITEM: QueueItem = QueueItem(EMPTY_DESCRIPTION, 0)
+    }
+
+    fun goToPrevious(): QueueItem {
+        return EMPTY_ITEM
+    }
+
+    fun goToNext(): QueueItem {
+        return EMPTY_ITEM
+    }
 }
