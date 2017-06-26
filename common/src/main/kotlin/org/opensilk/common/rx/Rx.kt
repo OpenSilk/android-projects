e5df9065767dcbedcea68a4171d24f6e34e47165
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2017-06-25 21:00:15 -0500

    common: add observable.subscribeignoreerror

--- common/src/main/kotlin/org/opensilk/common/rx/Rx.kt
+++ common/src/main/kotlin/org/opensilk/common/rx/Rx.kt
@@ -1,15 +1,27 @@
 package org.opensilk.common.rx
 
-import rx.Observable
-import rx.Subscription
+import rx.*
 import rx.android.schedulers.AndroidSchedulers
+import rx.functions.Action0
+import rx.functions.Action1
 
-fun notSubscribed(subscription: Subscription?): Boolean {
-    return subscription == null || subscription.isUnsubscribed
+fun Subscription?.isSubscribed(): Boolean {
+    return this != null && !this.isUnsubscribed
 }
 
-fun isSubscribed(subscription: Subscription?): Boolean {
-    return !notSubscribed(subscription)
+/**
+ * Null safe unsubscribe.
+ * @return true if unsubscribe() was called
+ */
+fun Subscription?.safeUnsubscribe(): Boolean {
+    return this?.let {
+        return@let if (!isUnsubscribed) {
+            unsubscribe()
+            true
+        } else {
+            false
+        }
+    } ?: false
 }
 
 fun unsubscribe(subscription: Subscription?) {
@@ -18,4 +30,39 @@ fun unsubscribe(subscription: Subscription?) {
 
 fun <T> Observable<T>.observeOnMainThread(): Observable<T> {
     return this.observeOn(AndroidSchedulers.mainThread())
+}
+
+fun <T> Observable<T>.subscribeIgnoreError(action: (T) -> Unit): Subscription {
+    return this.subscribe(object : SimpleSubscriber<T>() {
+        override fun onNext(t: T) {
+            action(t)
+        }
+    })
+}
+
+fun <T> Single<T>.subscribeIgnoreError(action: (T) -> Unit): Subscription {
+    return this.subscribe(object : SimpleSingleSubscriber<T>() {
+        override fun onSuccess(value: T) {
+            action(value)
+        }
+    })
+}
+
+open class SimpleSubscriber<T>: Subscriber<T>() {
+    override fun onError(e: Throwable?) {
+    }
+
+    override fun onCompleted() {
+    }
+
+    override fun onNext(t: T) {
+    }
+}
+
+open class SimpleSingleSubscriber<T>: SingleSubscriber<T>() {
+    override fun onSuccess(value: T) {
+    }
+
+    override fun onError(error: Throwable?) {
+    }
 }
