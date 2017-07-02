2aba71cffad15c3ebd6ff9ac0e819a3d74480ce4
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2017-07-02 14:51:49 -0500

    media-playback: add playback state extensions

---
+++ media-playback/src/main/java/org/opensilk/media/playback/funcs.kt
@@ -0,0 +1,28 @@
+package org.opensilk.media.playback
+
+import android.media.session.PlaybackState
+import android.media.session.PlaybackState.*
+
+/**
+ * Created by drew on 6/29/17.
+ */
+fun PlaybackState.hasAction(action: Long): Boolean {
+    return (actions and action) == action
+}
+fun PlaybackState._stringify(): String {
+    return when (state) {
+        STATE_NONE -> "STATE_NONE"
+        STATE_STOPPED -> "STATE_STOPPED"
+        STATE_PAUSED -> "STATE_PAUSED"
+        STATE_PLAYING -> "STATE_PLAYING"
+        STATE_FAST_FORWARDING -> "STATE_FAST_FORWARDING"
+        STATE_REWINDING -> "STATE_REWINDING"
+        STATE_BUFFERING -> "STATE_BUFFERING"
+        STATE_ERROR -> "STATE_ERROR"
+        STATE_CONNECTING -> "STATE_CONNECTING"
+        STATE_SKIPPING_TO_PREVIOUS -> "STATE_SKIPPING_TO_PREVIOUS"
+        STATE_SKIPPING_TO_NEXT -> "STATE_SKIPPING_TO_NEXT"
+        STATE_SKIPPING_TO_QUEUE_ITEM -> "STATE_SKIPPING_TO_QUEUE_ITEM"
+        else -> "UNKNOWN #" + state
+    }
+}
