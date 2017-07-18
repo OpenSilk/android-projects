be37d56cefe62f696dcc305560901fceb4a2db5f
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2017-07-18 16:22:51 -0500

    dagger: add fragmentscope

---
+++ common-dagger/src/main/java/org/opensilk/common/dagger/FragmentScope.kt
@@ -0,0 +1,10 @@
+package org.opensilk.common.dagger
+
+import javax.inject.Scope
+
+/**
+ * Created by drew on 7/15/17.
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class FragmentScope
