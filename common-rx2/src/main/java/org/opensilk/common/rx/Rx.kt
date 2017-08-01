b3c0c452ee7a4f1c9cce6e481a1c2a66ad3c8ba7
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2017-08-01 16:55:05 -0500

    add common-rx2

---
+++ common-rx2/src/main/java/org/opensilk/common/rx/Rx.kt
@@ -0,0 +1,38 @@
+package org.opensilk.common.rx
+
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+
+private val NIL_THROWABLE_CONSUMER = Consumer<Throwable> {  }
+
+/**
+ * Null safe dispose
+ * @return true if dispose() was called
+ */
+fun Disposable?.safeDispose(): Boolean {
+    return this?.let {
+        return@let if (!isDisposed) {
+            dispose()
+            true
+        } else false
+    } ?: false
+}
+
+fun <T> Observable<T>.observeOnMainThread(): Observable<T> {
+    return this.observeOn(AndroidSchedulers.mainThread())
+}
+
+fun <T> Observable<T>.subscribeIgnoreError(action: Consumer<T>): Disposable {
+    return this.subscribe(action, NIL_THROWABLE_CONSUMER)
+}
+
+fun <T> Single<T>.subscribeIgnoreError(action: Consumer<T>): Disposable {
+    return this.subscribe(action, NIL_THROWABLE_CONSUMER)
+}
+
+fun <T> Single<T>.observeOnMainThread(): Single<T> {
+    return this.observeOn(AndroidSchedulers.mainThread())
+}
