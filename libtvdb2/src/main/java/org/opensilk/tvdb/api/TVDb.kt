b85bee56969010110a02bf2ce820ba387ee4f375
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2017-08-06 15:55:50 -0500

    libtvdb: switch to singles

--- libtvdb2/src/main/java/org/opensilk/tvdb/api/TVDb.kt
+++ libtvdb2/src/main/java/org/opensilk/tvdb/api/TVDb.kt
@@ -3,6 +3,7 @@ package org.opensilk.tvdb.api
 import org.opensilk.tvdb.api.model.*
 import retrofit2.http.*
 import io.reactivex.Observable
+import io.reactivex.Single
 
 /**
  * Created by drew on 7/25/17.
@@ -10,29 +11,29 @@ import io.reactivex.Observable
 interface TVDb {
 
     @POST("/login")
-    fun login(@Body auth: Auth): Observable<Token>
+    fun login(@Body auth: Auth): Single<Token>
 
     @GET("/refresh_token")
-    fun refreshToken(@Header("Authorization") token: Token): Observable<Token>
+    fun refreshToken(@Header("Authorization") token: Token): Single<Token>
 
     @GET("/search/series")
-    fun searchSeries(@Header("Authorization") token: Token, @Query("name") name: String) : Observable<SeriesSearchData>
+    fun searchSeries(@Header("Authorization") token: Token, @Query("name") name: String) : Single<SeriesSearchData>
 
     @GET("/series/{id}")
-    fun series(@Header("Authorization") token: Token, @Path("id") id: Long): Observable<SeriesData>
+    fun series(@Header("Authorization") token: Token, @Path("id") id: Long): Single<SeriesData>
 
     @GET("/series/{id}/episodes")
     fun seriesEpisodes(@Header("Authorization") token: Token, @Path("id") id: Long,
-                       @Query("page") page: Int = 1): Observable<SeriesEpisodeData>
+                       @Query("page") page: Int = 1): Single<SeriesEpisodeData>
 
     @GET("/series/{id}/images/query")
     fun seriesImagesQuery(@Header("Authorization") token: Token, @Path("id") id: Long,
-                          @Query("keyType") keyType: String): Observable<SeriesImageQueryData>
+                          @Query("keyType") keyType: String): Single<SeriesImageQueryData>
 
     @GET("/updated/query")
-    fun updated(@Header("Authorization") token: Token, @Query("fromTime") fromTime: Long): Observable<UpdateData>
+    fun updated(@Header("Authorization") token: Token, @Query("fromTime") fromTime: Long): Single<UpdateData>
 
     @GET("/updated/query")
     fun updated(@Header("Authorization") token: Token, @Query("fromTime") fromTime: Long,
-                @Query("toTime") toTime: Long): Observable<UpdateData>
+                @Query("toTime") toTime: Long): Single<UpdateData>
 }
