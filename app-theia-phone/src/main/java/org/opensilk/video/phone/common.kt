c186690985d73b0dc20ee419932b374675eb0c7a
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2017-08-26 13:35:17 -0500

    theia: wip phone home screen

--- app-theia-phone/src/main/java/org/opensilk/video/phone/common.kt
+++ app-theia-phone/src/main/java/org/opensilk/video/phone/common.kt
@@ -15,6 +15,8 @@ import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.util.DiffUtil
+import android.support.v7.util.ListUpdateCallback
 import android.support.v7.widget.RecyclerView
 import android.view.MenuItem
 import android.view.View
@@ -23,6 +25,11 @@ import com.bumptech.glide.request.RequestOptions
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
+import io.github.luizgrp.sectionedrecyclerviewadapter.Section
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionParameters
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter
+import io.reactivex.Single
+import io.reactivex.disposables.Disposables
 import org.opensilk.media.*
 import org.opensilk.media.database.MediaDAO
 import org.opensilk.media.database.DocVideoChange
@@ -259,6 +266,8 @@ class ListItemViewHolder(val binding: RecyclerListItemBinding): BoundViewHolder(
         val desc = mediaRef.toMediaDescription()
         val imageResource = when (mediaRef) {
             is UpnpDeviceRef -> R.drawable.ic_lan_48dp
+            is StorageDeviceRef -> if (!mediaRef.id.isPrimary)
+                R.drawable.ic_usb_48dp else R.drawable.ic_folder_48dp
             is MediaDeviceRef,
             is FolderRef -> R.drawable.ic_folder_48dp
             is VideoRef -> R.drawable.ic_movie_48dp
@@ -268,7 +277,7 @@ class ListItemViewHolder(val binding: RecyclerListItemBinding): BoundViewHolder(
         binding.frame.setOnClickListener(this)
         //binding.frame.setOnLongClickListener(this)
         binding.titleString = desc.title.toString()
-        binding.subTitleString = desc.subtitle.toString()
+        binding.subTitleString = desc.subtitle?.toString() ?: ""
         if (!desc.iconUri.isEmpty()) {
             loadArtwork(desc.iconUri)
         } else {
@@ -319,4 +328,79 @@ class ListItemViewHolder(val binding: RecyclerListItemBinding): BoundViewHolder(
 interface MediaItemClickListener {
     fun onClick(mediaRef: MediaRef): Boolean
     fun onLongClick(mediaRef: MediaRef): Boolean
+}
+
+open class SwappingSectionedAdapter @Inject constructor(): SectionedRecyclerViewAdapter() {
+
+    private var mDisposable = Disposables.disposed()
+
+    fun swapList(tag: String, itemList: List<MediaRef>) {
+        val section = getSection(tag) as SwappingSection
+        handleSwap(section, ArrayList(section.itemList), ArrayList(itemList))
+    }
+
+    private fun handleSwap(section: SwappingSection, oldList: List<MediaRef>, newList: List<MediaRef>) {
+        mDisposable.dispose()
+        if (oldList.isEmpty()) {
+            section.itemList = newList
+            notifyItemRangeInsertedInSection(section, 0, newList.size)
+            return
+        }
+        if (newList.isEmpty()) {
+            section.itemList = emptyList()
+            notifyItemRangeRemovedFromSection(section, 0, oldList.size)
+            return
+        }
+        mDisposable = Single.fromCallable {
+            DiffUtil.calculateDiff(object: DiffUtil.Callback() {
+                override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                        oldList[oldItemPosition].id == newList[newItemPosition].id
+                override fun getOldListSize(): Int = oldList.size
+                override fun getNewListSize(): Int = newList.size
+                override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                        oldList[oldItemPosition] == newList[newItemPosition]
+            })
+        }.subscribeOn(AppSchedulers.background).observeOn(AppSchedulers.main).subscribe { result ->
+            section.itemList = newList
+            result.dispatchUpdatesTo(object: ListUpdateCallback {
+                override fun onChanged(position: Int, count: Int, payload: Any?) {
+                    notifyItemRangeChangedInSection(section, position, count)
+                }
+                override fun onMoved(fromPosition: Int, toPosition: Int) {
+                    notifyItemMovedInSection(section, fromPosition, toPosition)
+                }
+                override fun onInserted(position: Int, count: Int) {
+                    notifyItemRangeInsertedInSection(section, position, count)
+                }
+                override fun onRemoved(position: Int, count: Int) {
+                    notifyItemRangeRemovedFromSection(section, position, count)
+                }
+            })
+        }
+    }
+}
+
+abstract class SwappingSection(
+        params: SectionParameters,
+        private val headerItem: HeaderItem
+): Section(params) {
+
+    var itemList: List<MediaRef> = emptyList()
+
+    override fun getContentItemsTotal(): Int = itemList.size
+
+    override fun getItemViewHolder(view: View): RecyclerView.ViewHolder =
+            ListItemViewHolder(DataBindingUtil.bind(view))
+
+    override fun onBindItemViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ListItemViewHolder).bind(itemList[position])
+    }
+
+    override fun getHeaderViewHolder(view: View): RecyclerView.ViewHolder =
+            HeaderViewHolder(DataBindingUtil.bind(view))
+
+    override fun onBindHeaderViewHolder(holder: RecyclerView.ViewHolder) {
+        (holder as HeaderViewHolder).bind(headerItem)
+    }
+
 }
--- app-theia-phone/src/main/java/org/opensilk/video/phone/homescreen.kt
+++ app-theia-phone/src/main/java/org/opensilk/video/phone/homescreen.kt
@@ -1,52 +1,32 @@
 package org.opensilk.video.phone
 
-import android.arch.lifecycle.ViewModel
 import android.content.Context
-import android.databinding.DataBindingUtil
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.support.v7.util.DiffUtil
-import android.support.v7.util.ListUpdateCallback
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.android.AndroidInjection
 import dagger.android.ContributesAndroidInjector
-import dagger.multibindings.IntoMap
-import io.github.luizgrp.sectionedrecyclerviewadapter.Section
 import io.github.luizgrp.sectionedrecyclerviewadapter.SectionParameters
-import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter
 import org.opensilk.dagger2.ForApp
-import org.opensilk.media.MediaDeviceRef
+import org.opensilk.video.HomeViewModel
 import org.opensilk.video.LiveDataObserver
-import org.opensilk.video.ViewModelKey
 import javax.inject.Inject
 
-@Module(includes = arrayOf(HomeScreenHelperModule::class))
+const val CODE_NEED_PERMS = 1020
+
+@Module
 abstract class HomeScreenModule {
     @ContributesAndroidInjector
     abstract fun injector(): HomeActivity
-    @Binds @IntoMap @ViewModelKey(HomeScreenViewModel::class)
-    abstract fun viewModel(impl: HomeScreenViewModel): ViewModel
-}
-
-/**
- * Extra module required for @jvmStatic
- */
-@Module
-object HomeScreenHelperModule {
-    @Provides @JvmStatic
-    fun recyclerAdapter(): SectionedRecyclerViewAdapter = SectionedRecyclerViewAdapter()
 }
 
 class HomeActivity : DrawerActivity() {
 
-    private lateinit var mViewModel: HomeScreenViewModel
+    private lateinit var mViewModel: HomeViewModel
 
-    @Inject lateinit var mDeviceSection: DeviceSection
-    @Inject lateinit var mAdapter: SectionedRecyclerViewAdapter
+    @Inject lateinit var mAdapter: HomeAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
@@ -57,72 +37,48 @@ class HomeActivity : DrawerActivity() {
 
         mBinding.toolbar.title = getString(R.string.title_my_library)
 
-        mAdapter.addSection(mDeviceSection)
+        mViewModel = fetchViewModel(HomeViewModel::class)
 
-        mViewModel = fetchViewModel(HomeScreenViewModel::class)
-
-        mViewModel.servers.observe(this, LiveDataObserver {
-            handleNewServersList(mDeviceSection.devices, it)
+        mViewModel.devices.observe(this, LiveDataObserver {
+            mAdapter.swapList("devices", it)
+        })
+        mViewModel.needPermissions.observe(this, LiveDataObserver {
+            if (Build.VERSION.SDK_INT >= 23) {
+                requestPermissions(it, CODE_NEED_PERMS)
+            } // else pass
         })
 
-        mViewModel.fetchData()
+        mViewModel.subscribeData()
 
     }
 
-    private fun handleNewServersList(oldList: List<MediaDeviceRef>, newList: List<MediaDeviceRef>) {
-        mDeviceSection.devices = newList
-        DiffUtil.calculateDiff(object: DiffUtil.Callback() {
-            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                    oldList[oldItemPosition].id == newList[newItemPosition].id
-            override fun getOldListSize(): Int = oldList.size
-            override fun getNewListSize(): Int = newList.size
-            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                    oldList[oldItemPosition].meta == newList[newItemPosition].meta
-        }).dispatchUpdatesTo(object: ListUpdateCallback {
-            override fun onChanged(position: Int, count: Int, payload: Any?) {
-                mAdapter.notifyItemRangeChangedInSection(mDeviceSection, position, count)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            CODE_NEED_PERMS -> {
+                mViewModel.onGrantedPermissions(permissions.filterIndexed { index, _ ->
+                    grantResults[index] == PackageManager.PERMISSION_GRANTED
+                })
             }
-            override fun onMoved(fromPosition: Int, toPosition: Int) {
-                mAdapter.notifyItemMovedInSection(mDeviceSection, fromPosition, toPosition)
-            }
-            override fun onInserted(position: Int, count: Int) {
-                mAdapter.notifyItemRangeInsertedInSection(mDeviceSection, position, count)
-            }
-            override fun onRemoved(position: Int, count: Int) {
-                mAdapter.notifyItemRangeRemovedFromSection(mDeviceSection, position, count)
-            }
-        })
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 
+}
+
+class HomeAdapter @Inject constructor(
+        deviceSection: DeviceSection
+): SwappingSectionedAdapter() {
+    init {
+        addSection("devices", deviceSection)
+    }
 }
 
 class DeviceSection @Inject constructor(
         @ForApp context: Context
-): Section(
+): SwappingSection(
         SectionParameters.Builder(R.layout.recycler_list_item)
                 .headerResourceId(R.layout.recycler_header_item)
-                .build()
-) {
-
-    private val serversHeader = HeaderItem(context.getString(R.string.header_devices),
-            R.drawable.ic_server_network_48dp)
-
-    var devices = emptyList<MediaDeviceRef>()
-
-    override fun getContentItemsTotal(): Int = devices.size
-
-    override fun getHeaderViewHolder(view: View): RecyclerView.ViewHolder =
-            HeaderViewHolder(DataBindingUtil.bind(view))
-
-    override fun onBindHeaderViewHolder(holder: RecyclerView.ViewHolder) {
-        (holder as HeaderViewHolder).bind(serversHeader)
-    }
-
-    override fun getItemViewHolder(view: View): RecyclerView.ViewHolder =
-            ListItemViewHolder(DataBindingUtil.bind(view))
-
-    override fun onBindItemViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ListItemViewHolder).bind(devices[position])
-    }
-
-}
+                .build(),
+        HeaderItem(context.getString(R.string.header_devices),
+                R.drawable.ic_server_network_48dp)
+)
