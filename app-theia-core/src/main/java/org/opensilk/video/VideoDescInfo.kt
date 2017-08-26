fc77d470af5734cd9f2c054cfe66b82f994e036d
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2017-08-26 10:47:08 -0500

    theia: cleanup model

--- app-theia-core/src/main/java/org/opensilk/video/VideoDescInfo.kt
+++ app-theia-core/src/main/java/org/opensilk/video/VideoDescInfo.kt
@@ -1,7 +1,5 @@
 package org.opensilk.video
 
-import android.media.MediaDescription
-
 /**
  * Created by drew on 5/28/17.
  */
@@ -10,7 +8,3 @@ data class VideoDescInfo(
         val subtitle: String = "",
         val overview: String = ""
 )
-
-fun MediaDescription.videoDescInfo(): VideoDescInfo {
-    return VideoDescInfo(title.toString(), subtitle?.toString() ?: "", description?.toString() ?: "")
-}
